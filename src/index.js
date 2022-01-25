26afaff45f717604cb09768542cc91572b828024
Polcsi <pollakbence12@gmail.com>
2022-01-25 15:45:04 +0100

    Added Multiple Screen Layoutes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./css/index.css";
 import "./css/1366x768.css";
+import "./css/1280x1024.css";
+import "./css/1280x800.css";
+import "./css/1024x768.css";
+import "./css/mobile.css";
 import { AppProvider } from "./context";
 
 ReactDOM.render(
