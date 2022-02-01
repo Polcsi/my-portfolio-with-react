d466eb24de2d11f68fcecb0320d8a601a2692c2d
Polcsi <pollakbence12@gmail.com>
2022-02-01 15:33:24 +0100

    Added and improved high screen resolution looking

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./css/index.css";
+import "./css/2560x1440.css";
+import "./css/1920x1200.css";
 import "./css/1920x1080.css";
 import "./css/1680x1050.css";
 import "./css/1600x900.css";
